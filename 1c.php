f7d5c95e369e46a5a934aaad984aa9931cf04493
alexsasha <thebest-earnings@ya.ru>
2016-09-25 23:47:50 +0200

    3

---
+++ 1c.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * @author Хобта Сергей Сергеевич <hxss@ya.ru>
+ */
+
+require_once 'header.php';
+
+?>
+
+<section style="background-image: url('images/bg-3.jpg')">
+	<article>
+		<div class="img">
+			
+		</div>
+		<div class="text">
+			<h1 class="upc">Аренда и обслуживание 1с</h1>
+			<p>Начните работать со своей Базой 1С прямо сейчас!</p>
+			<p>Не нужно ничего покупать или настраивать.</p>
+			<p>Мы предоставим полностью готовую к работе 1С через 20 минут!</p>
+			<button class="btn">Заказать</button>
+		</div>
+	</article>
+</section>
+
+<section>
+	<article>
+		<div class="text hr">
+			<h3>Вам требуется 1С Бухгалтерия или Управление Торговлей ?</h3>
+			<p>Предлагаем Вам полностью сосредоточиться именно на Вашем Бизнесе, а о программах, настройках и последующей поддержке позаботится компания Первый Сервисный Провайдер!</p>
+			<p>Наши специалисты в максимально сжатые сроки настроят и предоставят Вам доступ к любым программам 1С, а Вам лишь останется начать их использовать.</p>
+		</div>
+		<div class="img" style="background-image: url('images/bg-sm-5.png')"></div>
+	</article>
+</section>
+
+<section class="bg-grey">
+	<article>
+		<div class="img bg-h bg-bottom" style="background-image: url('images/bg-sm-6.jpg')"></div>
+		<div class="text hr">
+			<p>Так как покупать и обслуживать отдельный сервер для 1С достаточно дорого, во многих компаниях его роль выполняет обычный офисный компьютер, совершенно не предназначенный для этого.</p>
+			<p>Его компоненты не рассчитаны на работу 24х7, а система хранения данных не резервирована. Хранение важной бухгалтерской информации на таком компьютере может привести к очень серьезным последствиям, вплоть до потери всей бухгалтерии.</p>
+			<p>Для таких организаций мы предлагаем серверы 1С в аренду. В этом случае, все ваши данные располагаются на надежных серверах, установленных в Дата-Центрах, а вы имеете к ним круглосуточный доступ из любой географической точки.</p>
+		</div>
+	</article>
+</section>
+
+<section class="bg-blue-0">
+	<header><a href="" class="btn">Узнать подробнее</a></header>
+</section>
+
+<section class="bg c" style="background-image: url('images/bg-4.jpg')">
+	<article class="pluses two-col">
+		<p style="background-image: url('images/icon-plus-12.png');"><i>Рабочие места 1С в аренду</i></p>
+		<p style="background-image: url('images/icon-plus-13.png');"><i>Установка и настройка систем 1С</i></p>
+		<p style="background-image: url('images/icon-plus-14.png');"><i>Обновление баз конфигураций</i></p>
+		<p style="background-image: url('images/icon-plus-15.png');"><i>Настройка резервного копирования</i></p>
+		<p style="background-image: url('images/icon-plus-16.png');"><i>Создание индивидуальных конфигураций</i></p>
+		<p style="background-image: url('images/icon-plus-17.png');"><i>Повышение быстродействия 1С</i></p>
+		<p style="background-image: url('images/icon-plus-18.png');"><i>Перенос 1С на терминальные серверы</i></p>
+		<p></p>
+	</article>
+</section>
+
+<section>
+	<header>
+		<h2>Цены на аренду 1С</h2>
+	</header>
+	<article class="flex price">
+		<div class="item">
+			<h3>1С в облаке</h3>
+			<p><span class="cost">1 200</span> ¤/мес.</p>
+			<ul class='long'>
+				<li>Количество пользователей: 1</li>
+				<li>Резервное копирование</li>
+				<li>Доступ из любой точки</li>
+				<li>Отказоустойчивые серверы</li>
+				<li>Программное обеспечение и обслуживание включено в стоимость</li>
+			</ul>
+			<button class="btn"></button>
+		</div>
+		<div class="item">
+			<h3>Виртуальное рабочее место с 1C</h3>
+			<p><span class="cost">1 550</span> ¤/мес.</p>
+			<ul class='long'>
+				<li>ОС Win Server 2008R2/2012</li>
+				<li>Office 2010/2013</li>
+				<li>Антивирус Forefront</li>
+				<li>15 Гб дискового пространства</li>
+				<li>Доступ из любой точки</li>
+				<li>Отказоустойчивые серверы</li>
+				<li>Бесплатное ПО</li>
+				<li>Ежедневное резерное копирование</li>
+				<li>Обслуживание, консультации пользователей</li>
+			</ul>
+			<button class="btn"></button>
+		</div>
+		<div class="item">
+			<h3>Сервер 1С</h3>
+			<p><span class="cost">7 000</span> ¤/мес.</p>
+			<ul class='long'>
+				<li>Количество пользователей: 8</li>
+				<li>Виртуальный выделенный сервер</li>
+				<li>Резервное копирование</li>
+				<li>Доступ из любой точки</li>
+				<li>Программное обеспечение и обслуживание включено в стоимость</li>
+			</ul>
+			<button class="btn"></button>
+		</div>
+	</article>
+</section>
+
+<section class="bg-grey">
+	<header>
+		<h2>Дополнительные услуги нашей компании</h2>
+	</header>
+	<article class="flex more-services">
+		<div class="item" style="background-image: url('images/icon-service-sm-1.png');">
+			<p>Обслуживание компьютеров</p>
+			<a href="" class="btn white"></a>
+		</div>
+		<div class="item" style="background-image: url('images/icon-service-sm-2.png');">
+			<p>Аренда 1C</p>
+			<a href="" class="btn white"></a>
+		</div>
+		<div class="item" style="background-image: url('images/icon-service-sm-3.png');">
+			<p>Проектирование сетевой инфраструктуры</p>
+			<a href="" class="btn white"></a>
+		</div>
+	</article>
+</section>
+
+<?php require_once 'footer.php'; ?>
